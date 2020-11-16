d56746b1345bf805deae450cbc1cc29459d2f139
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2020-11-17 00:25:44 +0530

    Create string Slicing and dictonary Accessing.py

---
+++ string Slicing and dictonary Accessing.py
@@ -0,0 +1,25 @@
+# string Slicing and dictonary Accessing
+
+# string
+my_variable = "techie_programmer"
+print(my_variable[:6])
+print(my_variable[7:])
+
+# list Slicing
+my_list = (1, 2, 3, 4, 5, "python")
+print(my_list[:3])
+
+# tuple Slicing
+my_tuple = (1, 2, 3, 4, 5, "python")
+print(my_tuple[5][-1])
+
+# accessing the dictonary
+my_dict = {"name": 'Techie', "lastname": 'Programmer', 'Age': 20}
+print(my_dict["Age"])
+# this returns the value associated with key
+# we will deal every one of it individually again
+# in up coming section
+
+# Sets basic
+my_sets = {1, 2, 3, 45, 231, 1, 2, 3}
+print(my_sets)
