000c6439952bfda062ebe09167178d106055ff80
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2020-11-16 09:43:43 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,63 @@
+"""
+Standard Data Types
+The data stored in memory can be of many types. For example,
+a person's age is stored as a numeric value and his or her
+address is stored as alphanumeric characters. Python has
+various standard data types that are used to define the
+operations possible on them and the storage method for
+each of them.
+
+Python has five standard data types −
+
+Numbers
+String
+List
+Tuple
+Dictionary
+
+"""
+# Data Types in Python
+
+# Int ex : 12,16,17,200
+
+var = 12
+print(type(var))
+
+# Float ex : 3.14
+
+var = 3.14
+print(type(var))
+
+# list ex : list of number 1,2,3,4,5
+
+list_var = [1,2,3,4,5]
+print(type(list_var))
+
+# Tuple  ex : same list
+
+tuple_var = (1,2,3,4,5)
+print(type(tuple_var))
+
+# Dictonary ex : key : value
+
+dict_var = {"name":"Techie","last Name":"Programmer","age":20}
+print(type(dict_var))
+
+# Sets ex : {1,2,4,5}
+
+set_var = {1,2,3,4,5,6,6,4,3}
+print(type(set_var))
+
+
+# other Data types In Python Are
+
+# string ex : "techie Programmer"
+string_var = "techie Programmer"
+print(type(string_var))
+
+# complex number ex : 3 + 4j
+complex_var = 3+4j
+print(type(complex_var))
+
+
+
