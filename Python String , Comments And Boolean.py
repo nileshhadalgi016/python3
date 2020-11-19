9c8936e0ebe318e2718c0a37a308be848d2f9246
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2020-11-19 09:46:34 +0530

    Create Python String , Comments And Boolean.py

---
+++ Python String , Comments And Boolean.py
@@ -0,0 +1,18 @@
+# strings
+
+my_var = 'pyt\'hon'
+new_var = "Prog'ramming"
+doc_var = '''this is a 
+doc string'''
+
+print(new_var)
+print(my_var)
+print(doc_var)
+
+# comments
+print("hello world") # this prints hello world
+
+# bool
+a = True + True
+print(a)
+print(False)
