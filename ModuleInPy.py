c9d15bde2c18591be176a06651de1ce872ab4ce9
Nilesh Hadalgi <57908107+nileshhadalgi016@users.noreply.github.com>
2020-11-28 09:45:27 +0530

    Create ModuleInPy.py

---
+++ ModuleInPy.py
@@ -0,0 +1,39 @@
+"""
+                Modules
+                    - Techie Programmer
+
+A file containing a set of functions you want to include in your application.
+
+
+
+create a myModule.py and add this 
+
+
+
+def hello(name):
+    print("Hello, " + name)
+
+
+author = "Techie Programmer"
+
+
+"""
+
+
+
+import myModule
+
+myModule.hello("techie")
+print(myModule.author)
+
+
+
+
+import tkinter as tk
+
+root = tk.Tk()
+
+
+root.mainloop()
+
+
